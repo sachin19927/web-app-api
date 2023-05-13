670b7c6c7f8baab4b4e6a17467363bb009ed020f
SACHIN HS <sachin19927@gmail.com>
2023-05-13 16:44:29 +0530

    kafka file to run in aws

--- src/main/java/com/service/developersjourneysource/model/LibraryRecord.java
+++ src/main/java/com/service/developersjourneysource/model/LibraryRecord.java
@@ -2,7 +2,7 @@ package com.service.developersjourneysource.model;
 
 import java.time.LocalDateTime;
 
-public record LibraryRecord(Long id,String title,String category,String author,String email,int year,double price,
+public record LibraryRecord(Long id,String title,String category,String author,String email,int year,double price,int pages,
 		LocalDateTime insertedDate,LocalDateTime modifiedDate) {
 
 }
